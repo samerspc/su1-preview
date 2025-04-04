56351083278d1ffc0db08d9d73327a25e83449b4
samerspc <mas.norvg13@gmail.com>
2025-04-04 21:12:16 +0300

    rm README

--- src/atomic/atoms/Slogan/Slogan.jsx
+++ src/atomic/atoms/Slogan/Slogan.jsx
@@ -1,5 +1,8 @@
 import styles from './styles.module.css';
 
 export const Slogan = () => {
-  return <h1 className={styles.h1}>СТРОТЕЛЬНОЕ УПРАВЛЕНИЕ №1</h1>;
-};
+  return (
+  <>
+    <h1 className={styles.h1}>СТРОТЕЛЬНОЕ УПРАВЛЕНИЕ №1</h1>
+  </>
+)};
